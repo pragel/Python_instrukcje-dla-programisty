006c786c1fc21aafd8c94d1805dfe70c2134fff1
pragel <janicki.dawid95@gmail.com>
2024-08-28 13:41:12 +0200

    Create wiki_python.py

---
+++ wiki_python.py
@@ -0,0 +1,12 @@
+(wyrażenie).title() #Dodaje wielką litrere do argumentu przypisanego do wyrażenia
+** #Jest to działanie potęgowania 
+lista = [] #Tworzenie listy w tym przypadku lista jest pusta możemy dodać do niej argumenty
+lista.pop() #Ta metoda usuwa ostatni argument z listy
+print(f'{lista} moje zdanie') #W tym przypadku możemy wyświetlić argumenty przypisane do wyrażenia
+lista.append('dwa') # do danie do końca listy argument dwa
+lista.insert(3, 'trzy') #Metoda insert posiada w sobie dwa argumenty pierwszymn określamy miejsce do którego ma dodać do listy a drugi odnośi sie co ma doda do listy 
+print(len(lista)) #Funkcja len zwróci nam ilość wyrażeń znajdujących się w liscie 
+del lista[] # funkcją del usuniemy wartość z listy jeżeli znamy ich położenie 
+print(sorted(lista)) # sortowanie listy alfabetycznie i wyświetlenie jej. Tą funkcją nie zmienimy na stałe kolejności listy
+lista.sort() # funkcja sort posortuje nam alfabetycznie listy na stałe 
+lista.sort(reverse = True) #sortuje odwrotnie alfabetycznie.
