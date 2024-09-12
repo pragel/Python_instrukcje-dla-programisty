8f8d48969c0aed54fa4f5d8b64c277b938959ba2
pragel <janicki.dawid95@gmail.com>
2024-09-12 21:43:59 +0200

    Update listy_petlefor_generowanie_liczb.py

--- listy_petlefor_generowanie_liczb.py
+++ listy_petlefor_generowanie_liczb.py
@@ -4,7 +4,9 @@ for value in range(1,21):
 # utworzenie pustej listy milion po czym za pomoca petli for dodanie do niej pokoleji liczb od 1 do miliona
 milion = []
 for value in range(1,1000001):
-    milion = value
+    milions = value
+    milion.append(milions)
+#print(milion)
 # utworzeni listy od 1 do miliona po czym wyświetlenie najmniejszej liczby najwekszej liczby oraz sumy liczb w zbiorze
 milion = list(range(1,1000001))
 x = min(milion)
@@ -14,4 +16,28 @@ z = sum(milion)
 print(x)
 print(y)
 print(z)
-    
+
+nieparzyste = list(range(1,20,2))
+for nieparzyst in nieparzyste:
+    print(nieparzyst)
+print("\n")
+
+potegi = list(range(3,31,))
+for potegowanie in potegi:
+    value = potegowanie **3
+print(potegi)
+print("\n") 
+#utworzenie pustej listy i wpisanie do niej liczb od 1 do 10 ale do potęgi 3
+szescian = []
+for value in range(1,11):
+    szesc = value **3
+    szescian.append(szesc)
+print(szescian)
+# ta sama instrukcja tylko krócej zapisana
+szescian = []
+for value in range(1,11):
+    szescian.append(value**3)
+print(szescian)
+# funkcja składana
+szescian = [value **3 for value in range(1,11)]
+print(szescian)
