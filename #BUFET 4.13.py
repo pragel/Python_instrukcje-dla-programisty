7d66f34e354b97ac9b6e9c98f3a403a3801d10bc
pragel <janicki.dawid95@gmail.com>
2024-09-13 19:41:57 +0200

    Create #BUFET 4.13.py

---
+++ #BUFET 4.13.py
@@ -0,0 +1,14 @@
+#BUFET 4.13
+#KROTKA
+
+menu = ('pizza', 'makaron', 'kluski śląskie', 'pomidorowa', 'naleśniki z bananami')
+print('MENU FOR RESTAURANT')
+for value in menu:
+    print(value.title())
+
+print('\n')
+
+menu = ('pizza', 'makaron', 'kiełbasa z grila', 'krem z pomidorów', 'naleśniki z bananami')
+print('MENU FOR RESTAURANT version 2')
+for value in menu:
+    print(value.title())
